10fa2119c54dd47e58ad13e2324e14c20946f5b4
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-06-08 09:25:26 +0530

    Create Comments.rb

---
+++ Basic Concepts/Comments.rb
@@ -0,0 +1,31 @@
+Comments
+
+PART 1
+
+Comments are lines of annotation within Ruby code that are ignored at program runtime.
+
+In Ruby, the hashtag symbol is used to create a single-line comment.
+
+For example:
+
+# printing some text
+puts "Hi there"
+
+Everything following the hashtag symbol on that line is ignored when the program is run.
+
+PART 2
+
+You can also create multi-line comments.
+
+Everything between the =begin and =end reserved words are considered a comment:
+
+=begin
+   This comment
+   spans multiple 
+   lines
+=end
+
+puts "Hello"
+
+
+Adding comments to your code is a good practice. It facilitates a clear understanding of the code for you and for others who read it.
