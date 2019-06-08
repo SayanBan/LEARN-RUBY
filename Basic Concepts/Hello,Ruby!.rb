6bf8c85877911b9034b0f9ef7e87545fd347bbde
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-06-08 09:22:39 +0530

    Update Hello,Ruby!.rb

--- Basic Concepts/Hello,Ruby!.rb
+++ Basic Concepts/Hello,Ruby!.rb
@@ -1,5 +1,7 @@
 Hello, Ruby!
 
+PART 1
+
 Let's create our very first Ruby program, the classic "Hello World" program.
 For this, we use the built-in puts method.
 
@@ -8,3 +10,27 @@ puts "Hello World"
 
 This code will output the text "Hello World" to the screen.
 All text values (strings) must be enclosed in single or double quotes.
+
+
+PART 2
+
+Hello, Ruby!
+
+Another method that can be used to display output to the screen is print.
+For example:
+
+print "Hello World"
+
+
+This code displays the same output as before, except that the puts automatically adds a new line after the output, while print does not.
+For example:
+
+puts "Hi"
+
+print "there"
+print "Ruby"
+
+Result:
+
+Hi
+thereRuby 
