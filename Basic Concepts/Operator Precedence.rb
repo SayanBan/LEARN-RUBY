6b085eb8d072bedc22f8cdc30b98c275b67d7292
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-06-08 09:43:06 +0530

    Create Operator Precedence.rb

---
+++ Basic Concepts/Operator Precedence.rb
@@ -0,0 +1,13 @@
+Operator Precedence
+
+Ruby evaluates a mathematical expression using an order of operations that is based on operator precedence. Exponentiation has the highest precedence followed by multiplication, division, and modulus from left to right, and then addition and subtraction from left to right.
+You can change the order of operations by using parentheses.
+
+For example:
+
+x = (3+2) * 4
+puts x
+# outputs 20
+
+
+Tap Try It Yourself to play around with the code!
