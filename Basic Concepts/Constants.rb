9fe4158e932a4d91aba823583696ac8fc24e558d
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-06-08 09:30:27 +0530

    Create Constants.rb

---
+++ Basic Concepts/Constants.rb
@@ -0,0 +1,12 @@
+Constants
+
+Variables beginning with a capital letter are called constants.
+The value of a constant variable cannot be changed once it has been assigned.
+
+For example:
+MyNum = 42
+
+# Trying to change the value produces a warning
+
+MyNum = 8
+# warning: already initialized constant MyNum 
