ce297123fb26ca2e98407fb8c0ed97333bdde28a
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-06-08 09:32:10 +0530

    Create Data Types.rb

---
+++ Basic Concepts/Data Types.rb
@@ -0,0 +1,23 @@
+Data Types
+
+All variables in Ruby can be of all data types.
+
+Ruby automatically determines data type by the value assigned to the variable.
+
+For example: 
+x = 42 # integer
+y = 1.58 # floating point value
+z = "Hello" # string
+
+You can reassign a different value to a variable at any time.
+
+To insert the value of a variable into a double quote string (a string is a sequence of characters, such as "Hello"), use the # symbol and curly brackets with the variable name.
+
+For example:
+age = 42
+puts "He is #{age} years old"
+
+# outputs "He is 42 years old"
+
+
+It is good practice to use the same data type in a single variable all over your application. It helps you to avoid mistakes.
