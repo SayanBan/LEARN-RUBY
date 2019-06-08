999b8bd2ce44dd0dd1ebfaec2aa83abffbfb464f
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-06-08 09:29:00 +0530

    Create Variables.rb

---
+++ Basic Concepts/Variables.rb
@@ -0,0 +1,18 @@
+Variables
+
+A variable is a named storage location for a value. It is called variable because the information stored in that location can be changed when the program is running.
+
+To assign a variable a value, use the equal sign.
+
+For example: 
+x = 8
+
+This assignment statement declares a variable named x and gives it the value 8. The equal sign is called the assignment operator.
+We can later use the variable name to access its value. For example, to output the value stored in the variable, we can use puts or print and refer to the variable name:
+
+x = 8
+puts x
+
+# outputs 8
+
+Variable names may consist of alphanumeric characters and the underscore character (_), but cannot begin with a capital letter or a number.
