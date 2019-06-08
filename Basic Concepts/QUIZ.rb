31b82880b5fe602d274bc52d7dd4dfb29bb8d964
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-06-08 10:04:40 +0530

    Create QUIZ.rb

---
+++ Basic Concepts/QUIZ.rb
@@ -0,0 +1,38 @@
+1) What is the output of this code?
+   
+   temp = "7"
+   temp += "2"
+   puts temp
+   
+Answer) 72
+
+2) Fill in the blanks to assign 42 to the variable, add 7 to it and output its value:
+
+x =_ 
+x_= 7
+puts _
+
+Answer) Fill in the blanks to make the text a comment:
+
+        ______
+        This is some 
+        text that spans 
+        multiple lines 
+        ____
+        
+Answer) =begin and -end
+
+3)     Fill in the blanks to embed the value of the variable in the string and output it to the screen.
+
+       name = "Amy"
+       msg = "Hi, my name is _ {____}"
+       puts____
+       
+ Answer) # , name , msg
+ 
+ 4)     What is the output of this code?
+        a = 4
+        puts a**2
+        
+ Answer)16
+   
