c7145d7b99ecf8d943dbcac66916a2c75d2c97a1
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-06-09 08:59:29 +0530

    Create QUIZ 2.rb

---
+++ Control structures/QUIZ 2.rb
@@ -0,0 +1,45 @@
+Q1) What is the result of (a&&b) || b, when a = true and b = false?
+A) False
+
+Q2) Fill in the blanks to create a valid if/else statement.
+A) status = 1
+  __status == 1
+  puts "Online"
+  else
+  puts "Offline"
+ ____
+
+Q3) How many numbers are there in the range (6..9)?
+A) 4
+
+Q4) How many lines of code will the following code output?
+a = 2
+loop do
+  puts a
+  a += 3
+  break if a > 8
+end
+
+A)  3
+
+Q5) Fill in the blanks to create a valid case statement.
+
+temp = 5
+___ temp
+when 0...19
+  puts "Low"
+ ____ 19...40
+  puts "Ok"
+else
+  puts "Wow"
+  ____
+  
+ A) Case, when, end
+ 
+ Q6) What is the output of this code?
+     x = 0
+     for i in 2...4
+     x+=i
+      end
+      puts x
+ A) 5
