306b434f1a9e83cbc045637d4830158e7b0b233b
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-09-09 22:56:06 +0530

    Create Parameters.rb

---
+++ Methods/Parameters.rb
@@ -0,0 +1,18 @@
+*PART 1*
+
+**A method declaration can include parameters, which you define inside parentheses after the method name.
+For example:
+def sqr(x)
+  puts x*x
+end
+
+sqr(8) 
+# outputs 64
+
+
+The defined sqr method has one parameter (called x) and outputs its square. You can name your parameters anything you like.
+When calling the method, we "pass" the actual parameter value to the method using parentheses.
+
+The actual parameter value is called an argument.
+
+So, in the above example, x is the parameter of the method, while 8 is the argumen
